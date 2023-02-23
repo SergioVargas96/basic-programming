4156656ea4945de62a231a860458875529ce0be4
Sergio_Vargas <sergio.vargas@grupor5.com>
2023-02-23 17:48:50 -0500

    solución ejercicios taller 2

---
+++ class_3/ejecicio_1_b.py
@@ -0,0 +1,34 @@
+numbers = []
+print("DIGITE 5 NUMEROS DEL 1 AL 5")
+
+num1 = int(input("Digite el primer numero: "))
+if num1 > 0 and num1 <=5:
+    numbers.append(str(num1))
+else:
+    exit()
+
+num2 = int(input("Digite el primer numero: "))
+if num2 > 0 and num2 <=5:
+    numbers.append(str(num2))
+else:
+    exit()
+
+num3 = int(input("Digite el primer numero: "))
+if num3 > 0 and num3 <=5:
+    numbers.append(str(num3))
+else:
+    exit()
+
+num4 = int(input("Digite el primer numero: "))
+if num4 > 0 and num4 <=5:
+    numbers.append(str(num4))
+else:
+    exit()
+
+num5 = int(input("Digite el primer numero: "))
+if num5 > 0 and num5 <=5:
+    numbers.append(str(num5))
+else:
+    exit()
+
+print("lista ", numbers)
