559048c986e14efe53322842d90709bdf9c18aca
Sergio_Vargas <sergio.vargas@grupor5.com>
2023-06-02 17:28:11 -0500

    add ejercice kata

---
+++ Kata/kata_bancaria.py
@@ -0,0 +1,51 @@
+import datetime
+
+transacciones = []
+
+
+def deposit():
+    while True:
+        consulta = input("¿Desea ingresar un depósito? (S/N): ")
+        if not consulta.isalpha() or consulta.upper() != 'S' and consulta.upper() != 'N':
+            print("El valor ingresado no es correcto")
+        if consulta.upper() == 'N':
+            break
+        elif consulta.lower() == 's':
+            monto = float(input("Ingrese monto a depositar: "))
+            if monto <= 0:
+                print("El monto ingresado no es correcto")
+            else:
+                date = datetime.datetime.now().strftime("%d/%m/%Y")
+                transacciones.append((date, monto))
+                print(f"Se ha depositado {monto} correctamente.")
+    print("el valor de la lista es: ", transacciones)
+    withdraw()
+
+
+def withdraw():
+    while True:
+        consulta_retiro = input("¿Desea hacer un retiro? (s/n): ")
+        if not consulta_retiro.isalpha() or consulta_retiro.upper() != 'S' and consulta_retiro.upper() != 'N':
+            print("El valor ingresado no es correcto")
+        if consulta_retiro.lower() == 'n':
+            print_statement()
+            break
+        elif consulta_retiro.lower() == 's':
+            retiro = float(input("Ingrese la cantidad del retiro: "))
+            if retiro <= 0 or retiro > sum([monto for date, monto in transacciones]):
+                print("Digito un valor incorrecto o no tiene fondos suficiente: ")
+            else:
+                date = datetime.datetime.now().strftime("%d/%m/%Y")
+                transacciones.append((date, - retiro))
+                print(f"Se ha retirado ${retiro} correctamente.")
+
+
+def print_statement():
+    saldo_actual = 0
+    print(f"Fecha|| Monto|| Balance")
+    for fecha, monto in transacciones:
+        saldo_actual += monto
+        print(f"{fecha}|| {monto}|| {saldo_actual}")
+
+
+deposit()
