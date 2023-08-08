da684d91fda6ed6e98a20ea23f9172da4ffcf7dc
Sergio_Vargas <sergio.vargas@grupor5.com>
2023-08-08 16:15:20 -0500

    add test kata bancaria

--- course/tests/test_Kata_bancaria.py
+++ course/tests/test_Kata_bancaria.py
@@ -1,1 +1,21 @@
-from course.src.Kata
+from course.src.Kata.kata_bancaria_one import Transactions, Account
+
+def test_create_transactions():
+    transaction = Transactions("10/01/2012", 1000)
+    assert transaction.date == "10/01/2012"
+    assert transaction.amount == 1000
+
+
+def test_deposit():
+    account = Account()
+    account.deposit("10/01/2012", 1000)
+    assert len(account.full_transactions) == 1
+    assert account.full_transactions[0].date == "10/01/2012"
+    assert account.full_transactions[0].amount == 1000
+
+
+def test_withdraw():
+    account = Account()
+    account.withdraw("11/01/2012", -500)
+    assert account.full_transactions[0].date == "11/01/2012"
+    assert account.full_transactions[0].amount == 500
